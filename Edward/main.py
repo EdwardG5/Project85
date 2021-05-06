0af623b629ba85e3a82648dc93ea862cdeb050ff
EdwardG5 <edwardgaremo@gmail.com>
2021-05-06 13:11:38 -0400

    Latest versions of all files. Mostly small changes I think?

--- Edward/main.py
+++ Edward/main.py
@@ -55,18 +55,6 @@ if __name__ == '__main__':
 
     p()
 
-    if N == 1:
-        print(f"Will compress: {N} genome. ", end="")
-    else:
-        print(f"Will compress: {N} genomes. ", end="")
-
-    if PARALLEL:
-        print(f"In parallel, using {cpu_count()} processors.")
-    else:
-        print(f"Sequentially.")
-
-    p("\nInitializing state...\n")
-
     # Count already compressed
     alreadyCompressed = listdir(compressed_file_location)
     alreadyCompressed = len(list(filter(lambda x: x[-4:] == ".bin", alreadyCompressed)))
@@ -83,6 +71,19 @@ if __name__ == '__main__':
     
     N = len(toCompress)
 
+    if N == 1:
+        print(f"Will compress: {N} genome. ", end="")
+    else:
+        print(f"Will compress: {N} genomes. ", end="")
+
+    if PARALLEL:
+        # This was actually changed, see later note. 
+        print(f"In parallel, using {cpu_count()} processors.")
+    else:
+        print(f"Sequentially.")
+
+    p("\nInitializing state...\n")
+
     # Begin compression
     p("Beginning compression...\n")
     start = time.time()
@@ -91,9 +92,16 @@ if __name__ == '__main__':
         # we need to stop the script, we don't throw away massive amounts of
         # work. We also send an email notification to inform the user of
         # progress.
-        for iteration in range(ceil(len(toCompress)/1000)):
-            part = toCompress[(iteration)*1000:(iteration+1)*1000]
-            with get_context("spawn").Pool() as pool:
+        batchSize = 1000
+        for iteration in range(ceil(len(toCompress)/batchSize)):
+            part = toCompress[(iteration)*batchSize:(iteration+1)*batchSize]
+            # For some reason a deadlock seems to occur when you just let this
+            # be the default ".Pool()" using all 40 processors. Unclear why. It
+            # also only seem to have appeared on the second day of running the
+            # program. Never managed to figure out what the issue is -
+            # restricting the andrew machines to 30/40 of their cores seemed to
+            # work.  
+            with get_context("spawn").Pool(30) as pool:
                 returnValues = pool.map(transform, part)
             print("\nBeginning storage of genomes, writing files...\n")
             for (ID, path, val) in returnValues:
@@ -108,13 +116,13 @@ if __name__ == '__main__':
             # Send an email notification.
             msg = notify.message(
                 subject="Compression progress report",
-                text=(f'Another 1000 genomes compressed.\n'
-                    f'Only another {len(toCompress)-1000*(iteration+1)} to go!')
+                text=(f'Another {batchSize} genomes compressed.\n'
+                    f'Only another {max(0, len(toCompress)-batchSize*(iteration+1))} to go!')
             )
             notify.send(msg)        
     else:
         # print("Sequential code needs to be fixed, nothing will be done.")
-        returnValues = list(map(lambda x: storeAsBinary(reference.seq, x[0], compressed_file_location+x[1]+".bin"), toCompress))
+        returnValues = list(map(lambda x: storeAsBinary(x[1], x[2], x[3]), toCompress))
     end = time.time()
     elapsedTime = end - start
 
